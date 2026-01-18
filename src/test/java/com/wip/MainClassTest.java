2fe289159b3429f8820d672a235a4755410e8acb
Jan1user490 <Jan1user490@rpsconsulting.in>
2026-01-19 00:35:02 +0530

    new editt

---
+++ src/test/java/com/wip/MainClassTest.java
@@ -0,0 +1,49 @@
+package com.wip;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MainClassTest {
+
+    MainClass mc;
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("From SetupClass method");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("From Setup method");
+        mc = new MainClass();
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("From add method");
+        assertEquals(60, mc.add(20, 20, 20));
+    }
+
+    @Test
+    public void testMultiply() {
+        System.out.println("From multiply method");
+        // 10 * 20 = 200
+        assertEquals(200, mc.multiply(10, 20));
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("From TEAR Down  method");
+        mc = null;
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("From tearDownClass method");
+    }
+}
